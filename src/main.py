b9743e50ac96a9efcde67644ef4c29f8ad92e310
Noxmain <noxmain@icloud.com>
2025-06-26 01:54:40 +0200

    Added comments

--- src/main.py
+++ src/main.py
@@ -17,13 +17,14 @@ input = torch.randn((1, 3, image_size, image_size)) # sample random noise
 # output generation
 current = input
 for t in tqdm(scheduler.timesteps):
-    with torch.no_grad():
-        predicted_noise = model(current, t).sample
-        current = scheduler.step(predicted_noise, t, current).prev_sample
+    with torch.no_grad(): # no training
+        predicted_noise = model(current, t).sample # inference
+        current = scheduler.step(predicted_noise, t, current).prev_sample # denoise
 
 # output
 # output = pipe().images[0]
-output = (current / 2 + 0.5).clamp(0, 1)
-output = output.permute(0, 2, 3, 1).numpy()[0]
-output = Image.fromarray((output * 255).round().astype("uint8"))
-output.save("output/output.png")
+output = (current / 2 + 0.5).clamp(0, 1) # map from [-1,1] to [0,1]
+output = output.permute(0, 2, 3, 1).numpy()[0] # change order of tensor
+output = (output * 255).round().astype("uint8") # convert to image values
+output = Image.fromarray(output) # convert to image
+output.save("output/output.png") # save image
